d7e5a291e5876a9052217f74729f20b9be47eaff
Phuoc Nguyen <shishima21@gmail.com>
2023-10-25 18:05:49 +0700

    convert css to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    fontFamily: {
+      default: ['Nunito', 'sans-serif']
+    },
+    fontSize: {
+      '3xs': '10px',
+      '2xs': '11px',
+      xs: '12px',
+      sm: '14px',
+      base: '16px',
+      lg: '18px',
+      xl: '20px',
+      '2xl': '24px',
+      '3xl': '30px',
+      '4xl': '36px',
+      '5xl': '48px',
+      '6xl': '56px',
+      '7xl': '70px',
+      '22px': '22px'
+    },
+    extend: {
+      colors: {
+        dimgray: '#696969'
+      }
+    }
+  },
+  plugins: []
+}
