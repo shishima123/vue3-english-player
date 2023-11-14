28a311fef034b0752f78b7e26c27e29bd187ad98
Phuoc Nguyen <shishima21@gmail.com>
2023-11-14 16:52:36 +0700

    fix playlist overflow

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -21,7 +21,7 @@ defineEmits(['play'])
 
 <template>
   <section
-    class="flex flex-col flex-nowrap w-full mx-auto bg-white overflow-hidden scrollbar hover:overflow-auto relative h-0 row-start-2 row-end-3 col-start-2 col-end-3 md:h-[var(--playlist-height)] md:rounded md:shadow-md transition-[height] duration-[350ms] ease-linear"
+    class="flex flex-col flex-nowrap w-full mx-auto bg-white overflow-auto md:overflow-hidden scrollbar md:hover:overflow-auto relative h-0 row-start-2 row-end-3 col-start-2 col-end-3 md:h-[var(--playlist-height)] md:rounded md:shadow-md transition-[height] duration-[350ms] ease-linear"
     :class="{ 'playlist-lyrics-section-active': showPlaylistState }"
     ref="wrapper"
   >
