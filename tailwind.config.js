b4dba0313c811a9d141150f0c751e4ab7c76ed0e
Phuoc Nguyen <shishima21@gmail.com>
2024-09-14 01:57:04 +0700

    chore: update dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   corePlugins: {
     preflight: false
   },
